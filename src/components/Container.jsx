633465d9419fdf8be6c6b16433b3ef9785aa80b2
chamathr <chamatht20@gmail.com>
2022-11-30 14:30:42 +0530

    changes

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import 'grapesjs/dist/css/grapes.min.css';
 import 'grapesjs-preset-newsletter/dist/grapesjs-preset-newsletter.css';
 import grapesjs from 'grapesjs';
@@ -6,22 +6,45 @@ import 'grapesjs-preset-newsletter';
 
 const Container = () => {
 
+    let editor = null
+
     useEffect(() => {
-        const editor = grapesjs.init({
+        editor = grapesjs.init({
             container: '#gjs',
-            plugins: ['gjs-preset-newsletter']
+            plugins: ['gjs-preset-newsletter'],
+
+            storageManager: {
+                type: 'local',
+                autosave: true,
+                autoload: true, 
+                stepsBeforeSave: 0, 
+                options: {
+                    local: { 
+                        key: 'gjsProject',
+                    },
+                }
+            }
         });
-        editor.BlockManager.add('my-first-block', {
-            label: 'Simple block',
-            content: `<div class="my-block margin-only" 
-                style="box-sizing: border-box; height: 100px; margin: 10vh 0 0 0; text-align: center;">
-                This is a simple block 2</div>`,
+
+        editor.on('component:update', e => {
+            const pageBody = editor.runCommand('gjs-get-inlined-html')
+            localStorage.setItem("pageContent", pageBody);
         });
+
     })
 
+    const handleSubmit = () => {
+        const pageContent = localStorage.getItem("pageContent");
+        console.log(pageContent)
+    }
+
     return (
-        <div id="gjs">
-        </div>
+        <>
+            <div id="gjs">
+            </div>
+
+            <button onClick={handleSubmit}>Add page</button>
+        </>
     );
 
 }
